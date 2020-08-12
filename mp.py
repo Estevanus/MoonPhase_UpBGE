37c4fe09eb84b178eb5b12d317537ef904eb04d5
Estevanus <oscartoreh@gmail.com>
2020-08-12 09:39:05 +0800

    Add files via upload

---
+++ mp.py
@@ -0,0 +1,33 @@
+import time
+from mathutils import Euler, Matrix
+from math import radians
+
+
+lastNewMoonRecord = 1595294380.7988243#in seconds
+moonCycle = 29.53#in days
+
+
+mc = moonCycle * 24 * 60 * 60
+
+
+def cek(cont):
+	thisTime = time.time()
+	
+	x = thisTime - lastNewMoonRecord
+
+	moonPhase = (x%mc) / 60 / 60 / 24 / 29.53
+	
+	#cont.owner["pos"] = moonPhase
+	#print(moonPhase)#for debugging
+	
+	own = cont.owner
+	if "moon" in own:
+		moon = own.scene.objects[own['moon']]
+		
+		#rot = 45
+		rot = 360 * moonPhase
+		r = radians(rot)
+		el = Euler((0, 0, r))
+		#print(el)
+		moon.worldOrientation = el.to_matrix()
+		
